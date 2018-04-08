859a7638a5ab23f0d8cd75ec2b52d06431e1e68c
jose miguel parra tomas <parra@debian>
2018-04-08 15:45:34 +0200

    add mf3 a tabla

--- src/zdMakeTabLista.c
+++ src/zdMakeTabLista.c
@@ -10,19 +10,22 @@ int mf1GeneraMakefile();
 int mf1GeneraTabla();
 int mf2GeneraMakefile();
 int mf2GeneraTabla();
+int mf3GeneraMakefile();
+int mf3GeneraTabla();
 int mfBGeneraMakefile();
 int mfBGeneraTabla();
 zdMakeTab_t zdMakeTabList[]=
 {
 {"1","normal","makefile normal",mf1GeneraTabla,mf1GeneraMakefile},
 {"2","lib","libreria",mf2GeneraTabla,mf2GeneraMakefile},
+{"3","binario","binario instalable",mf3GeneraTabla,mf3GeneraMakefile},
 {"B","bib","biblioteka",mfBGeneraTabla,mfBGeneraMakefile}
 };
 
 
 zdMakeTabLista_t zdMakeTabLista=
 {
-3, zdMakeTabList
+4, zdMakeTabList
 };
 
 zdMakeTabLista_t *zdMakeListaGet()
